15b35af7d839b51a56d68318e4aef12a9692377e
Cruzbaby <84595207+Cruzbaby@users.noreply.github.com>
2023-10-08 07:44:54 +0700

    Setup

--- server.js
+++ server.js
@@ -3,16 +3,25 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path')
 const app = express();
-const port = 3000
+const port = process.env.PORT;
+const StoreRouter = express.Router();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public")));
 
+app.set("views", "./src/views")
+app.set("view engine", "ejs")
+
 app.get("/", (req, res) => {
-
-    res.send("Hello Cruz Developer");
-
+    res.render('index',{username: 'Cruz Developer'});
 })
+
+StoreRouter.route("/").get((req, res) => {
+    res.send("Hello World !! In Store Page")
+});
+
+app.use("/store", StoreRouter)
+
 
 app.listen(port, () => {
     console.log("App Running In Port", port);
